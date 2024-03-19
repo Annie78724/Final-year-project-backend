f4f6b23576279781a9eaf0b050ee83d0da215f37
Annie78724 <ananya2003pal@gmail.com>
2024-03-19 09:10:22 +0530

    vercel1

--- index.js
+++ index.js
@@ -290,6 +290,7 @@ app.get("/api/get-cited-by-information", async (req, res) => {
 });
 
 
-app.listen(5000, (req, res) => {
-  console.log("Server is listening to port 5000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
